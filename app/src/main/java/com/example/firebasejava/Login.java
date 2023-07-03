5fe5ac5622a173a8e542b9ff4b06a72dd7bafa7f
Joaquin Valeroso <darthjoaqui@outlook.com>
2023-07-03 22:59:21 +0800

    should be functional

--- app/src/main/java/com/example/firebasejava/Login.java
+++ app/src/main/java/com/example/firebasejava/Login.java
@@ -5,21 +5,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.TextPaint;
+import android.text.TextUtils;
 import android.text.method.LinkMovementMethod;
 import android.text.style.ClickableSpan;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
+    TextInputEditText editEmail, editPass;
+    Button buttonLogIn;
+    FirebaseAuth mAuth;
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editEmail = findViewById(R.id.emailIN);
+        editPass = findViewById(R.id.passwordIN);
+        buttonLogIn = findViewById(R.id.loginBTN);
+        mAuth = FirebaseAuth.getInstance();
+
 
         TextView textView = findViewById(R.id.regText);
         String text = "click here to Register.";
@@ -40,5 +69,39 @@ public class Login extends AppCompatActivity {
         ss.setSpan(cs1, 13,22, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         textView.setText(ss);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        buttonLogIn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String email, password;
+                email = String.valueOf(editEmail.getText());
+                password = String.valueOf(editPass.getText());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(Login.this,"email cannot be empty.",Toast.LENGTH_SHORT).show();
+                }
+                if (TextUtils.isEmpty(password)){
+                    Toast.makeText(Login.this,"password cannot be empty.",Toast.LENGTH_SHORT).show();
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),"Welcome Back.", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Login.this, "bad credentials.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/firebasejava/Register.java
+++ app/src/main/java/com/example/firebasejava/Register.java
@@ -3,10 +3,19 @@ package com.example.firebasejava;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,6 +30,18 @@ public class Register extends AppCompatActivity {
     TextInputEditText editEmail, editPass;
     Button buttonReg;
     FirebaseAuth mAuth;
+    ProgressBar progressBar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -31,9 +52,12 @@ public class Register extends AppCompatActivity {
         editPass = findViewById(R.id.passwordIN);
         buttonReg = findViewById(R.id.regBTN);
         mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
                 String email, password;
                 email = String.valueOf(editEmail.getText());
                 password = String.valueOf(editPass.getText());
@@ -49,6 +73,7 @@ public class Register extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(Register.this, "Account Created.",
                                             Toast.LENGTH_SHORT).show();
                                 } else {
@@ -61,6 +86,24 @@ public class Register extends AppCompatActivity {
             }
         });
 
+        TextView textView = findViewById(R.id.logText);
+        String text = "click here to log in.";
 
+        SpannableString ss = new SpannableString(text);
+
+        ClickableSpan cs1 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                startActivity(new Intent(Register.this, Login.class));
+            }
+            @Override
+            public void updateDrawState(TextPaint ds){
+                super.updateDrawState(ds);
+                ds.setColor(Color.rgb(250,107,212));
+            }
+        };
+        ss.setSpan(cs1, 13,21, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        textView.setText(ss);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
