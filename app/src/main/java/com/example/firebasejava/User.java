e0e726f8437f58853c3eed92845988008b83ab2c
Joaquin Valeroso <darthjoaqui@outlook.com>
2023-07-04 17:27:38 +0800

    now with working database integ

--- app/src/main/java/com/example/firebasejava/User.java
+++ app/src/main/java/com/example/firebasejava/User.java
@@ -10,5 +10,27 @@ public class User {
         this.pass = password;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getDisplay() {
+        return display;
+    }
+
+    public void setDisplay(String display) {
+        this.display = display;
+    }
 }
